4491fb0b48fee87aa9a132da929e9bbda7d2255c
Shane Brennan <ShaneB@bellevueclub.com>
2025-05-29 12:12:34 -0700

    First Commit, had to ignore some files

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Rating = "R",
+                    Watched = false,
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Rating = "PG",
+                    Watched = true,
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Rating = "PG",
+                    Watched = true,
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Rating = "PG-13",
+                    Watched = false,
+                    Price = 3.99M
+                },
+                new Movie
+                {
+                    Title = "Marvel Avengers Engame",
+                    ReleaseDate = DateTime.Parse("2019-4-26"),
+                    Genre = "Action",
+                    Rating = "PG-13",
+                    Watched = true,
+                    Price = 4.99m
+                },
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    Genre = "Romantic Comedy",
+                    Rating = "R",
+                    Watched = true,
+                    Price = 7.99M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
